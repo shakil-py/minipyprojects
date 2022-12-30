5094735a9a71b25ce70e8a5d14e04e42a26a5ecb
shakil-py <shakilhaquepy@gmail.com>
2022-12-30 21:31:41 +0600

    add

---
+++ mini-1/number-ges.py
@@ -0,0 +1,24 @@
+import random
+top_of_range=input("Type a number: ")
+if top_of_range.isdigit():
+    top_of_range=int(top_of_range)
+
+    if top_of_range<=0:
+        print("Please type a number larger than 0 next time.")
+else:
+    print("Please type a number next time.")
+    quit()
+random_number = random.randint(0,top_of_range)
+
+while True:
+    user_ges=input("Make A Guess")
+    if user_ges.isdigit():
+        user_ges=int(user_ges)
+    else:
+        print("Please type anumber next time.")
+        continue
+    if user_ges==random_number:
+        print("You got it!")
+    else:
+        print("You got it wrong !")
+    break
