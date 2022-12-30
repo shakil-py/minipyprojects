5deaee12df8f20fa76c0b85faf9ef171559bfd52
shakil-py <shakilhaquepy@gmail.com>
2022-12-30 21:14:48 +0600

    add

--- mini-1/mini-main-1.py
+++ mini-1/mini-main-1.py
@@ -5,20 +5,26 @@ playing = input("Do you want to play? ")
 if playing.lower() != "yes":
     quit()
 print("okay let's play")
+socre = 0
 answer = input("What dose CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("Correct !")
+    socre+=1
 else:
     print("Incorrect !")
 
 answer = input("What dose Graphics Card stand for? ")
 if answer.lower() == "grafical processing unit":
     print("Correct !")
+    socre+=1
 else:
     print("Incorrect !")
 
 answer = input("What dose RAM stand for? ")
-if answer.lower() == "random access processing unit":
+if answer.lower() == "random accesse memory unit":
     print("Correct !")
+    socre+=1
 else:
-    print("Incorrect !")
+    print("Incorrect !") 
+print("You got "+ str(socre)+" question correct !")
+print("you got "+str((socre/4)*100)+"%.")
