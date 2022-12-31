82b8081327ba234854da350d95ccdc34fbe74a45
shakil-py <shakilhaquepy@gmail.com>
2022-12-31 16:13:13 +0600

    add

---
+++ mini-1/rok-paper-sissor.py
@@ -0,0 +1,32 @@
+import random
+user_wins = 0
+computer_wins = 0
+options = ["rock", "paper", "scissors"]
+options[0] =
+while true:
+    user_input = input("Type rock/paper/sessior o Q to quit").lower()
+    if user_input == "q":
+        break
+
+    if user_input in options:
+        continue
+    random_number = random.randint(0, 2)
+
+    computer_pick = options[random_number]
+    print("computer picked", computer_pick + ".")
+    if user_input == "rock" and computer_pick == "scissors":
+        print("you won !")
+        user_wins += 1
+    elif user_input == "paper" and computer_pick == "rock":
+        print
+        ("you won !")
+        user_wins += 1
+    elif user_input=="scissors"and computer_pick=="paper":
+        print("you won!")
+        user_winss+=1
+    else:
+        print("you lost !")
+        computer_wins+=1
+
+
+print("GoodBye!")
