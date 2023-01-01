00b1cf00bb00a292043d1454f6ca0ed7bd04d4bf
shakil-py <shakilhaquepy@gmail.com>
2023-01-01 10:40:59 +0600

    add

--- mini-1/chooseyourownadventure.py
+++ mini-1/chooseyourownadventure.py
@@ -4,7 +4,7 @@ print("welcome",name.capitalize(),"to this adventure")
 answer= input("You are on a dirt roade,it has come to an end and you can go left or right: ").lower()
 
 if answer=="left":
-    answer=input("You come to a river ,you can walk around it or swim accrose the river: ").lower
+    answer=input("You come to a river ,you can walk around it or swim accrose the river: ").lower()
 
     if answer=="swim":
         print()
