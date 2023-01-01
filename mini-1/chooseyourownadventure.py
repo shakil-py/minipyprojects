500debf997b44c0d4a35b3b4995d5c568a3ec7b7
shakil-py <shakilhaquepy@gmail.com>
2023-01-01 10:40:00 +0600

    add

---
+++ mini-1/chooseyourownadventure.py
@@ -0,0 +1,19 @@
+name=input("Type Your name: ")
+print("welcome",name.capitalize(),"to this adventure")
+
+answer= input("You are on a dirt roade,it has come to an end and you can go left or right: ").lower()
+
+if answer=="left":
+    answer=input("You come to a river ,you can walk around it or swim accrose the river: ").lower
+
+    if answer=="swim":
+        print()
+    if answer=="walk":
+        print()
+    else:
+        print("Not a valid option. you lose.")
+
+elif answer == "right":
+    print()
+else:
+    print("Mot a valid option. You lose!")
